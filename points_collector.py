6ae0f08103180a0f3c911690fec08950108483cc
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-05-23 12:26:28 +0300

    Make the player move with the mouse
    The player is a box for now and moves on the screen by simply moving the mouse.

--- points_collector.py
+++ points_collector.py
@@ -3,6 +3,8 @@ import random
 
 pygame.init()
 
+clock = pygame.time.Clock()
+
 WHITE_COLOR = (255, 255, 255)
 BLUE_COLOR = (30, 144, 255)
 
@@ -20,15 +22,24 @@ class Player(pygame.sprite.Sprite):
         self.rect = pygame.rect.Rect((500, 500), (40, 40))
         self.rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    def move(self):
+        pygame.mouse.set_visible(False)
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        self.rect.center = (mouse_x, mouse_y)
+
+
+player = Player()
 
 while True:
-    player = Player()
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.MOUSEMOTION:
+            player.move()
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             sys.exit()
 
     screen.fill(WHITE_COLOR)
     pygame.draw.rect(screen, BLUE_COLOR, player.rect)
     pygame.display.flip()
+    clock.tick(20)
